8ba665733d031c03fd1e47fa6b5af2361421754c
DanielCripps94 <dan-cripps@hotmail.co.uk>
2020-01-07 15:11:21 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // ensuring the whole dom content is loaded before below code runs. We can keep our script at the top of the html
+  
+const inputs = document.querySelector('#create-task-form')
+
+// const selectedInput = inputs[1]
+
+inputs.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+})
+
+
+
+
 });
